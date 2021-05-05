51d12a430ae38e83c55cfd5efebf8fa3ee8b65fa
Daniela Escanella <dani@MacBook-Pro-de-Daniela-94.local>
2021-05-05 19:56:09 -0400

    Crear funcion App

--- lab-04.js
+++ lab-04.js
@@ -1,14 +1,33 @@
- 
+ var prompt = require('prompt-sync')({sigint:true});
+
  const functionView = counter => `Count: ${counter}`;
 
  const functionUpdate = (msg, counter) => {
      if (msg=="+"){
-        return counter
+        return counter+1
      } else{
-         return counter*-1
+         return counter-1
      }
- }
+ };
 
- console.log(functionView(3));
- console.log(functionUpdate("-" , 3));
+ const functionApp = (counter) =>{
+     if (counter=="q") {
+         return counter
+     } else{
+        const currentView = functionView(counter);
+        console.clear();
+        console.log(currentView);
+        console.log();
+        console.log ('(+) (-)');
+        console.log();
+        console.log('(q) for quit');
+        const msg = prompt("What would you do? ")
+        counter = (functionUpdate(msg , counter));
+        functionApp(counter);
+
+     }
+ };
+ 
+ 
+
 
