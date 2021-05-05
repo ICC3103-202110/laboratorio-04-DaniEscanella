6e33a4ccf41b7757e86855b07cd8550f254ffaec
Daniela Escanella <dani@MacBook-Pro-de-Daniela-94.local>
2021-05-05 19:31:01 -0400

    Funcion View y Update

--- lab-04.js
+++ lab-04.js
@@ -1,3 +1,14 @@
  
- console.log('Hola')
+ const functionView = counter => `Count: ${counter}`;
+
+ const functionUpdate = (msg, counter) => {
+     if (msg=="+"){
+        return counter
+     } else{
+         return counter*-1
+     }
+ }
+
+ console.log(functionView(3));
+ console.log(functionUpdate("-" , 3));
 
